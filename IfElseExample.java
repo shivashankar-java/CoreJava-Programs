702d2821fa63fa9678650486caee0e0e8e6882b3
unknown <shivashankar.damidi@gmail.com>
2024-11-14 15:26:42 +0530

    project Added

---
+++ IfElseExample.java
@@ -0,0 +1,29 @@
+package ControlStatements;
+
+//public class IfElseExample {
+//	
+//	    public static void main(String[] args) {
+//	        int num = 10;
+//	        
+//	        if (num > 5) {
+//	            System.out.println("Number is greater than 5");
+//	        }
+//	        else {
+//	            System.out.println("Number is not greater than 5");
+//	        }
+//	    }
+//	}
+
+public class IfElseExample {
+    public static void main(String[] args) {
+        int number = -5;
+        
+        
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else {
+            System.out.println("The number is negative.");
+        }
+    }
+}
+
