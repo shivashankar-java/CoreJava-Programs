e3e4e28ab0c2137afb3eaafdecf53f0e46c33fb9
unknown <shivashankar.damidi@gmail.com>
2024-12-16 13:10:55 +0530

    Added ControlStatements

---
+++ ControlStatements/IfElseIfExample.java
@@ -0,0 +1,31 @@
+package ControlStatements;
+
+//public class IfElseIfExample {
+//	
+//	    public static void main(String[] args) {
+//	        int num = 10;
+//	        
+//	        if (num > 15) {
+//	            System.out.println("Number is greater than 15");
+//	        } else if (num > 5) {
+//	            System.out.println("Number is greater than 5 but less than or equal to 15");
+//	        } else {
+//	            System.out.println("Number is less than or equal to 5");
+//	        }
+//	    }
+//	}
+
+public class IfElseIfExample {
+    public static void main(String[] args) {
+        int number = 0;
+        
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+}
+
